be91fcf04b22b0ef634c1a6534a46f73945e86e4
Pascal Sauvetre <pascal.sauvetre@lm.se>
2016-01-13 14:56:17 +0100

    Hämta koden från min egen repo

--- modules/generatedata/manifests/init.pp
+++ modules/generatedata/manifests/init.pp
@@ -2,7 +2,7 @@ class generatedata {
   vcsrepo { "/vagrant/webroot/generatedata":
     ensure => present,
     provider => git,
-    source => 'https://github.com/benkeen/generatedata.git',
+    source => 'https://github.com/psauvetre/generatedata.git',
     revision => 'master',
     owner    => 'root',
     group => 'root'
